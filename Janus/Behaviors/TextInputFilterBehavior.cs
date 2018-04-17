93d5464b2692e9b2d26fa9e9dc224cce0eb1fbd3
Hendrik Lösch <loeschhendrik@gmail.com>
2018-04-17 13:41:34 +0200

    Added Behaviors

---
+++ Janus/Behaviors/TextInputFilterBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Janus.Behaviors
+{
+    public class TextInputFilterBehavior : Behavior<TextBox>
+    {
+        static readonly List<Key> ValidKeys = new List<Key>() {Key.NumPad0, Key.NumPad1, Key.NumPad2, Key.NumPad3, Key.NumPad4, Key.NumPad5, Key.NumPad6, Key.NumPad7, Key.NumPad8, Key.NumPad9 };
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+        }
+
+        private static void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = !ValidKeys.Contains(e.Key);
+        }
+    }
+}
